ce382183c1b1a6920d0f3015ac4d9a79b5ec9770
lukasvicente <lucasvicente2@gmail.com>
2018-01-29 10:45:08 -0200

    adc webservice

---
+++ app/control/UtilWebservice.php
@@ -0,0 +1,67 @@
+<?php
+
+class UtilWebservice
+{
+
+	//public static $HOST_NAME = "http://devceres.govrn/";
+	//public static $PROJECT_NAME = "ceres/";
+	//public static $WEBSERVICE_DIRECTORY = "app/services/mulher/";
+
+	public static $HOST_NAME = "http://localhost/";
+	public static $PROJECT_NAME = "cereswc/";
+	public static $WEBSERVICE_DIRECTORY = "app/services/mulher/";
+
+
+	public static $SUCCESS_TAG = "success";
+	public static $DADOS_TAG   = "dados";
+	public static $ERROR_TAG   = "error";
+
+	public static function callWebservice( $webserviceName, $parameters, $method )
+	{
+
+		$getData = http_build_query( $parameters );
+
+		$opts = array( 
+				'http' => array( 
+						'method'  => $method, 
+						'content' => $getData ) );
+
+		$context  = stream_context_create( $opts );
+
+		$jsonData = file_get_contents( UtilWebservice::getWebserviceURL( $webserviceName ), false, $context  );
+
+		/*var_dump( $jsonData );
+		exit; */
+
+		return json_decode( $jsonData, true );
+
+	}
+	
+	public static function callWebserviceFile( $webserviceName, $parameters, $method )
+	{
+	
+		$getData = http_build_query( $parameters );
+	
+		$opts = array( 'http' => array( 
+				'method'  => $method, 
+				'headers' => 'Content-Type: multipart/form-data',
+				'content' => $getData ) );
+	
+		$context  = stream_context_create( $opts );
+	
+		$jsonData = file_get_contents( UtilWebservice::getWebserviceURL( $webserviceName ), false, $context  );
+	
+		return json_decode( $jsonData, true );
+	
+	}
+
+	public static function getWebserviceURL( $webserviceName )
+	{
+
+		return UtilWebservice::$HOST_NAME . UtilWebservice::$PROJECT_NAME . UtilWebservice::$WEBSERVICE_DIRECTORY . $webserviceName . ".class.php";
+
+	}
+
+}
+
+ ?>
