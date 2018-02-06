8c630541b54331e2369700d6c14ee62a05d78162
Lukas_PCassema <lucasvicente2@gmail.com>
2018-02-06 13:48:05 -0200

    documentos

--- Pages/DocumentosView.php
+++ Pages/DocumentosView.php
@@ -12,28 +12,74 @@
     </div>
   </div>
 
+<?php 
 
-  <div class="container">
-    <div class="section">
+require_once "app/control/UtilWebservice.php"; 
+  
+        $GRUPO_SERVICO_WEBSERVICE =
+        UtilWebservice::$HOST_NAME .
+        UtilWebservice::$PROJECT_NAME .
+        UtilWebservice::$WEBSERVICE_DIRECTORY .
+        "DocumentoWebservice.class.php";
 
-  <div class="row">
-  <div class="section scrollspy">
-    <div class="col s12">
+        $jsonData = file_get_contents( $GRUPO_SERVICO_WEBSERVICE );
+        $jsonServicos = json_decode( $jsonData, true );
+        $successServico = $jsonServicos[UtilWebservice::$SUCCESS_TAG];
+        $dadosJson = $jsonServicos[UtilWebservice::$DADOS_TAG];
+        
 
-      <ul class="tabs tab-demo z-depth-1">
-        <li class="tab col s3"><a href="#balancete">Balancetes</a></li>
-        <li class="tab col s3"><a href="#test2">Portarias</a></li>
-        <li class="tab col s3"><a href="#test3">Eleições</a></li>
-        <li class="tab col s3"><a href="#test4">Outros</a></li>
 
-      </ul>
-    </div>
-    
-    <div id="balancete" class="col s12">
+
+        if( $successServico == 1 )
+        {
+        
+            mountDocumentos( $dadosJson );
+             
+        }else if( $successServico == 2 )
+        {
+             
+            //new TMessage( 'INFO', 'Nenhum valor foi encontrado.' );
+             
+        }else if( $successServico == 0 )
+        {
+             
+            //new TMessage( 'INFO', 'Ocorreu um problema, tente novamente.' );
+             
+        }
+
+function mountDocumentos( $values )
+    {
+      $div = '  
+      <div class="container">
+      <div class="section">
+      <div class="row">
+      <div class="section scrollspy">
+      <div class="col s12">
+      <ul class="tabs tab-demo z-depth-1">';
+
+      echo $div;
+
+
+      foreach ($values as $key) {
+
+      $div = '<li class="tab col s3"><a href="#'.$key['tipo'].'">'.$key['tipo'].'</a></li>';
+     
+      echo $div;
+
+      }
+
+      echo "</ul></div>";
+  }
+
+
+?>
+
+
+    <div id="BALANCETES" class="col s12">
     <ul class="collapsible" data-collapsible="accordion">
     <li>
       <div class="collapsible-header"><i class="material-icons">date_range</i>2017</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      <div class="collapsible-body"><span>Janeiro</span></div>
     </li>
     <li>
       <div class="collapsible-header"><i class="material-icons">date_range</i>2016</div>
@@ -46,7 +92,7 @@
   </ul>
     </div>
 
-    <div id="test2" class="col s12">Test 2</div>
+    <div id="PORTARIAS" class="col s12">Test 2</div>
     <div id="test3" class="col s12">Test 3</div>
     <div id="test4" class="col s12">Test 4</div>
   </div>
