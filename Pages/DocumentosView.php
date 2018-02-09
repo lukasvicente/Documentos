3191ff51f83d5237df2b0ab1061ea303b6d54707
Lukas_PCassema <lucasvicente2@gmail.com>
2018-02-09 13:15:19 -0200

    doc

--- Pages/DocumentosView.php
+++ Pages/DocumentosView.php
@@ -16,7 +16,62 @@
 
 require_once "app/control/UtilWebservice.php"; 
 
-function mountDocumentoJson()
+function mountTipoDocumentoJson()
+    {
+        $GRUPO_SERVICO_WEBSERVICE =
+        UtilWebservice::$HOST_NAME .
+        UtilWebservice::$PROJECT_NAME .
+        UtilWebservice::$WEBSERVICE_DIRECTORY .
+        "TipoDocumentoWebservice.class.php";
+
+        $jsonData = file_get_contents( $GRUPO_SERVICO_WEBSERVICE );
+        $jsonServicos = json_decode( $jsonData, true );
+        $successServico = $jsonServicos[UtilWebservice::$SUCCESS_TAG];
+        $dadosJson = $jsonServicos[UtilWebservice::$DADOS_TAG];
+
+        if( $successServico == 1 )
+        {
+        
+            mountTipoDocumentos( $dadosJson );
+             
+        }else if( $successServico == 2 )
+        {
+             
+            //new TMessage( 'INFO', 'Nenhum valor foi encontrado.' );
+             
+        }else if( $successServico == 0 )
+        {
+             
+            //new TMessage( 'INFO', 'Ocorreu um problema, tente novamente.' );
+             
+        }
+    
+    }
+
+function mountTipoDocumentos( $values )
+    {
+      $div = '  
+      <div class="container">
+      <div class="section">
+      <div class="row">
+      <div class="section scrollspy">
+      <div class="col s12">
+      <ul class="tabs tab-demo z-depth-1">';
+
+      echo $div;
+
+      foreach ($values as $value) {
+
+      $div = '<li class="tab col s3"><a href="#'.$value['nome'].'">'.$value['nome'].'</a></li>';
+
+      echo $div;
+
+      }
+
+      echo "</ul></div>";
+  }
+
+  function mountDocumentoJson()
     {
         $GRUPO_SERVICO_WEBSERVICE =
         UtilWebservice::$HOST_NAME .
@@ -50,57 +105,36 @@ function mountDocumentoJson()
 
 function mountDocumentos( $values )
     {
-      $div = '  
-      <div class="container">
-      <div class="section">
-      <div class="row">
-      <div class="section scrollspy">
-      <div class="col s12">
-      <ul class="tabs tab-demo z-depth-1">';
+     
+      $div = '    
+      <div id="BALANCETES" class="col s12">
+      <ul class="collapsible" data-collapsible="accordion">';
 
       echo $div;
-
-      $socio = '';
-
+      $ano = '';
       foreach ($values as $value) {
 
-      $div = '<li class="tab col s3"><a href="#'.$value['tipo'].'">'.$value['tipo'].'</a></li>';
+      if ($ano <> $value['ano']) {
 
-     if ($socio != $value['tipo']) {
-      
-      $socio = $value['tipo'];
+      $ano = $value['ano'];
+      $div = '    
+      <li>
+      <div class="collapsible-header"><i class="material-icons">date_range</i>'.$value['ano'].'</div>
+      <div class="collapsible-body"><span> <a href="'.$value['link'].'" target="_blank">'.$value['mes'].'</a></span></div>
+      </li>';
+
       echo $div;
-
         }
-      
+
       }
 
       echo "</ul></div>";
   }
 
+  print_r( mountTipoDocumentoJson() );
   print_r( mountDocumentoJson() );
 
 ?>
-
-    <div id="BALANCETES" class="col s12">
-
-    <ul class="collapsible" data-collapsible="accordion">
-    <li>
-      <div class="collapsible-header"><i class="material-icons">date_range</i>2017</div>
-      <div class="collapsible-body"><span>Janeiro</span></div>
-
-    </li>
-    <li>
-      <div class="collapsible-header"><i class="material-icons">date_range</i>2016</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
-    <li>
-      <div class="collapsible-header"><i class="material-icons">date_range</i>2015</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
-  </ul>
-
-    </div>
 
     <div id="PORTARIAS" class="col s12">Test 2</div>
     <div id="test3" class="col s12">Test 3</div>
