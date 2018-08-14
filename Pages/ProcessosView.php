cdede48c64815a117a307e3803b20755f3e10d93
Lucas Vicente <lucasvicente2@gmail.com>
2018-08-14 09:32:12 -0300

    nova cor para o slider

---
+++ Pages/ProcessosView.php
@@ -0,0 +1,40 @@
+<div class="container">
+    <div class="section">
+        <nav class="teal lighten-2">
+            <div class="nav-wrapper">
+                <div class="col s12">
+                    <a href="index.php" class="breadcrumb"><i style="margin-left:20px;" class="material-icons">home</i></a>
+                    <a href="index.php?page=Processos" class="breadcrumb">Processos</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+
+    <table class="highlight">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Item Name</th>
+            <th>Item Price</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+            <td>$0.87</td>
+        </tr>
+        <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+            <td>$3.76</td>
+        </tr>
+        <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+            <td>$7.00</td>
+        </tr>
+        </tbody>
+    </table>
